8b0cef5a8b074c617a80e9e51ccb251e0faa3026
marc <mbgraham2000@gmail.com>
2020-09-16 14:37:34 -0700

    update gateway installer -- use mozilla-gateway-lite

--- extras/gateway/install_gateway.sh
+++ extras/gateway/install_gateway.sh
@@ -4,6 +4,13 @@ then
     echo "The installer expects to find ~/.m2ag-labs/ssl/rootca.crt"
     exit
 fi
+echo "get the code"
+mkdir mozilla-iot
+git https://github.com/m2ag-labs/mozilla-gateway-lite mozilla-iot
+cd mozilla-iot/gateway
+npm install -y
+cd
+
 echo 'setup systemd'
 sudo cp "$HOME/m2ag-labs/installer/extras/gateway/mozilla-gateway.service" /etc/systemd/system/mozilla-gateway.service
 sudo sed -i 's*--HOME--*'"$HOME"'*g' /etc/systemd/system/mozilla-gateway.service
