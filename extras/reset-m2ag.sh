9b3799050aef7c7d2b885134365f49f199a9f6fd
marc <mbgraham2000@gmail.com>
2020-09-17 16:36:17 -0700

    update  rest-m2ag, gateway, install sh

--- extras/reset-m2ag.sh
+++ extras/reset-m2ag.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # for development -- reset m2ag-labs code but don't do a reinstall of all the services.
 # does not delete .m2ag-labs
+sudo systemctl stop m2ag-thing
+sudo systemctl stop m2ag-api
 rm -rf "$HOME/m2ag-labs"
-rm -rf "$HOME/m2ag-labs/installer"
-rm -rf "$HOME/m2ag-labs/client"
 echo 'install services'
-git clone https://github.com/m2ag-labs/m2ag-iot-thing.git "$HOME/m2ag-labs"
-git clone https://github.com/m2ag-labs/m2ag-iot-installer.git "$HOME/m2ag-labs/installer"
-git clone https://github.com/m2ag-labs/m2ag-iot-client.git "$HOME/m2ag-labs/client"
+git clone https://github.com/m2ag-labs/m2ag-thing.git "$HOME/m2ag-labs"
+git clone https://github.com/m2ag-labs/m2ag-thing-installer.git "$HOME/m2ag-labs/installer"
+git clone https://github.com/m2ag-labs/m2ag-thing-client.git "$HOME/m2ag-labs/client"
 #copy default config:
 cp "$HOME"/m2ag-labs/installer/thing/config_template/server.json "$HOME"/m2ag-labs/config/server.json
 cp "$HOME"/m2ag-labs/installer/thing/config_template/component_map.json "$HOME"/m2ag-labs/config/component_map.json
@@ -18,3 +18,4 @@ mkdir "$HOME"/m2ag-labs/config/available/components
 mkdir "$HOME"/m2ag-labs/config/available/things
 mkdir "$HOME"/m2ag-labs/device/hardware/components
 mkdir "$HOME"/m2ag-labs/device/things/components
+sudo systemctl start m2ag-api
