b06f1091deed88cbff2b7d608d180617c4e849dd
marc <mbgraham2000@gmail.com>
2021-01-05 11:22:10 -0800

    updates for raspicam

--- extras/raspicam/install.sh
+++ extras/raspicam/install.sh
@@ -4,18 +4,20 @@
 #TODO: setup motion file
 
 # get the needed components.
-wget https://raw.githubusercontent.com/m2ag-labs/m2ag-iot-things/master/components/raspipwm/init_raspi_hw_pwm.py
+sudo pip3 install wiringpi
+
+wget https://raw.githubusercontent.com/m2ag-labs/m2ag-things/master/components/raspipwm/init_raspi_hw_pwm.py
 mv init_raspi_hw_pwm.py "$HOME/m2ag-labs/device/hardware/components/"
-wget https://raw.githubusercontent.com/m2ag-labs/m2ag-iot-things/master/components/raspipwm/raspipwm.json
+wget https://raw.githubusercontent.com/m2ag-labs/m2ag-things/master/components/raspipwm/raspipwm.json
 mv raspipwm.json "$HOME/m2ag-labs/config/available/components/"
-wget https://raw.githubusercontent.com/m2ag-labs/m2ag-iot-things/master/components/raspipwm/raspipwm.py
+wget https://raw.githubusercontent.com/m2ag-labs/m2ag-things/master/components/raspipwm/raspipwm.py
 mv raspipwm.py "$HOME/m2ag-labs/device/hardware/components/"
 #add the sentry specific things
-wget https://raw.githubusercontent.com/m2ag-labs/m2ag-iot-things/master/things/raspicam/raspicam.json
+wget https://raw.githubusercontent.com/m2ag-labs/m2ag-things/master/things/raspicam/raspicam.json
 sed -i 's*--HOSTNAME--*'"$HOSTNAME"'*g' raspicam.json
 mv raspicam.json "$HOME/m2ag-labs/config/available/things/"
 # get the ui files
-wget https://raw.githubusercontent.com/m2ag-labs/m2ag-iot-things/master/extras/raspicam/raspicam.html
+wget https://raw.githubusercontent.com/m2ag-labs/m2ag-things/master/extras/raspicam/raspicam.html
 mv raspicam.html "$HOME/m2ag-labs/client/ui/"
 
 
