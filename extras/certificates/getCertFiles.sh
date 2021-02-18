a9875c96eb6092cb37fd3d5c5ef5e685c7c5053c
marc <mbgraham2000@gmail.com>
2021-02-17 17:42:38 -0800

    remove names from server.csr.cnf

--- extras/certificates/getCertFiles.sh
+++ extras/certificates/getCertFiles.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
+# https://raw.githubusercontent.com/m2ag-labs/m2ag-thing-installer/main/extras/certificates/getCertFiles.sh
 if [ ! -d "$HOME/.m2ag-labs/certs" ]; then
     mkdir "$HOME/.m2ag-labs"
     mkdir "$HOME/.m2ag-labs/certs"
 fi
 echo "$HOME/.m2ag-labs/certs"
-wget ""
+mkdir "templates"
+mkdir "devices"
+wget "https://raw.githubusercontent.com/m2ag-labs/m2ag-thing-installer/main/extras/certificates/createServerCert.sh"
+wget "https://raw.githubusercontent.com/m2ag-labs/m2ag-thing-installer/main/extras/certificates/createRootCa.sh"
+wget "https://raw.githubusercontent.com/m2ag-labs/m2ag-thing-installer/main/extras/certificates/templates/server.csr.cnf"
+wget "https://raw.githubusercontent.com/m2ag-labs/m2ag-thing-installer/main/extras/certificates/templates/v3.ext"
+mv server.csr.cnf templates/server.csr.cnf
+mv v3.ext templages/v3.ext
