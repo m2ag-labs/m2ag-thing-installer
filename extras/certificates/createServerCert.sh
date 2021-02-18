2334c12379eeee2c2daaf32940b051b1d7632d38
marc <mbgraham2000@gmail.com>
2021-02-17 17:31:53 -0800

    add simple cert management

---
+++ extras/certificates/createServerCert.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+if [[ "$1" == "" ]]
+then
+    echo "please supply the hostname"
+    exit
+fi
+
+mkdir devices/"$1"
+echo "$1"
+cp templates/server.csr.cnf server.csr.cnf
+cp templates/v3.ext v3.ext
+# mac needs the "" after -i
+sed -i "" 's*--HOSTNAME--*'"$1"'*g' v3.ext
+sed -i "" 's*--HOSTNAME--*'"$1"'*g' server.csr.cnf
+openssl req -new -sha256 -nodes -out server.csr -newkey rsa:2048 -keyout server.key -config server.csr.cnf
+openssl x509 -req -in server.csr -CA rootca/rootCA.pem -CAkey rootkey/rootCA.key -CAcreateserial -out server.crt -days 825 -sha256 -extfile v3.ext
+mv server.crt devices/"$1"/server.crt
+mv server.key devices/"$1"/server.key
+rm server.csr
+rm server.csr.cnf
+rm v3.ext
---
+++ extras/certificates/getCertFiles.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+if [ ! -d "$HOME/.m2ag-labs/certs" ]; then
+    mkdir "$HOME/.m2ag-labs"
+    mkdir "$HOME/.m2ag-labs/certs"
+fi
+echo "$HOME/.m2ag-labs/certs"
+wget ""
